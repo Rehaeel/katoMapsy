09796b0c0681d1ef156841dc9e259c76ceb263b4
Rehaeel <rafal@prokopiak.pl>
2022-01-24 22:19:38 +0100

    set server core

---
+++ api.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.DB_PORT;
+
+app.use(cors());
+app.use(express.json());
+const mystery = mysql.createConnection({
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
+});
+
+/////////// STAN
+app.get('/state', (req, res) => {
+	mystery.query('SELECT * FROM mysteriesState', (err, result) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.send(result);
+		}
+	});
+});
+
+/////////// STAN
+app.get('/state', (req, res) => {
+	mystery.query('SELECT * FROM mysteriesState', (err, result) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.send(result);
+		}
+	});
+});
+
+app.listen(process.env.PORT || PORT, () => {
+	console.log(`My DB run on ${PORT} port`);
+});
