95305c0220232827ec3f99e3b67834b37a33abd4
Rehaeel <rafal@prokopiak.pl>
2022-02-26 12:50:05 +0100

    add intro film

--- src/components/dashboard/introduction/introduction.jsx
+++ src/components/dashboard/introduction/introduction.jsx
@@ -61,13 +61,14 @@ const Introduction = () => {
 						aplikacji. Zapraszam!
 					</p>
 					<iframe
-						width='560'
+						width='540'
 						height='315'
-						src='https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ'
+						src='https://www.youtube.com/embed/5grGgPw21wo'
 						title='YouTube video player'
 						frameBorder='0'
 						allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
 						allowFullScreen></iframe>
+
 					<FormControlLabel
 						control={
 							<Checkbox
